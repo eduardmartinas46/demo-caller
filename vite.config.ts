766866f351a5a6913a083173264e0c6b1a84aa63
eduardmartinas46 <eduard.business46@gmail.com>
2025-08-19 11:00:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+
+  export default {
+  build: {
+    rollupOptions: {
+      external: ['src/main.tsx']
+    }
+  }
+}
 });
